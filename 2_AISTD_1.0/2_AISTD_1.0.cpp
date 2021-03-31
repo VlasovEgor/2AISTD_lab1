edea441a3b9d5902d02e5fe5147eab8b1a0d4cc1
VlasovEgor <73856832+VlasovEgor@users.noreply.github.com>
2021-03-31 21:16:31 +0300

    Add files via upload

---
+++ 2_AISTD_1.0/2_AISTD_1.0.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Red_Black_Tree.h"
+using namespace std;
+
+int main()
+{
+	RBTree<int, char> tree;
+	tree.Insert(6, 'a');
+	tree.Insert(2, 'b');
+	tree.Insert(4, 'c');
+	tree.Remove(2);
+	tree.Insert(8, 'd');
+	tree.Insert(3, 'e');
+	tree.Insert(5, 'f');
+	tree.Print();
+}
+
